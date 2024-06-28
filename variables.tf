10fba83fab043458c40628830f53e61ae5b47324
Sabyasachi Swain <sabyasachiswain@Sabyasachis-MacBook-Air.local>
2024-06-28 21:50:13 +0530

    add github action for CICD

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "ami_value" {
-    description = "value for ami"
+  description = "value for ami"
 }
 
 variable "instance_type" {
-    description = "value for instance_type"
-  
+  description = "value for instance_type"
+
 }
 
 variable "tag_value" {
-    description = "value for tag_value"
-  
+  description = "value for tag_value"
+
 }
